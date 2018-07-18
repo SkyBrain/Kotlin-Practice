623d32feea5671826f34aa9e8b7aa70209df4f73
SampathKumar <sampath53@hotmail.com>
2018-07-18 19:53:10 -0400

    Kotlin Notification App Completed Version

--- NCApp/app/src/main/java/com/example/sampath/ncapp/MainActivity.kt
+++ NCApp/app/src/main/java/com/example/sampath/ncapp/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.example.sampath.ncapp
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import android.widget.EditText
+import com.example.sampath.ncapp.R.id.btnMovieSettings
+import com.example.sampath.ncapp.R.id.btnWatchMovie
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var notificationHandler:NotificationHandler
+
+    companion object {
+        val WATCH_MOVIE_NOTIFICATION_ID = 1000
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            this.btnMovieSettings.id -> {
+                openNotification(NotificationHandler.WATCH_MOVIE_NOTIFICATION_CHANNEL_ID)
+            }
+            this.btnWatchMovie.id -> {
+                postNotificationToUserDevice(WATCH_MOVIE_NOTIFICATION_ID,edtWatchMovie.text.toString())
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        notificationHandler = NotificationHandler(this)
         setContentView(R.layout.activity_main)
+        btnWatchMovie.setOnClickListener(this)
+        btnMovieSettings.setOnClickListener(this)
     }
+
+    fun postNotificationToUserDevice(notificationID:Int, titleText:String){
+        var notificationBuilder:Notification? = null
+        when(notificationID){
+            WATCH_MOVIE_NOTIFICATION_ID ->{
+                notificationBuilder = notificationHandler.createAndReturnWatchMovieNotification(titleText,"This is a great movie.")
+            }
+        }
+
+        if (notificationBuilder != null){
+            notificationHandler?.notifyTheUser(WATCH_MOVIE_NOTIFICATION_ID,notificationBuilder)
+        }
+    }
+
+    fun openNotification(id:String){
+        var settingsIntent = Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS)
+        settingsIntent.putExtra(Settings.EXTRA_APP_PACKAGE,packageName)
+        settingsIntent.putExtra(Settings.EXTRA_CHANNEL_ID,id)
+        startActivity(settingsIntent)
+    }
+
 }
--- NCApp/app/src/main/java/com/example/sampath/ncapp/NotificationHandler.kt
+++ NCApp/app/src/main/java/com/example/sampath/ncapp/NotificationHandler.kt
@@ -6,11 +6,13 @@ import android.app.NotificationManager
 import android.content.Context
 import android.content.ContextWrapper
 import android.graphics.Color
-import android.os.Parcel
-import android.os.Parcelable
 
 class NotificationHandler(context:Context): ContextWrapper(context) {
-    var notificationManager:NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+    var notificationManager:NotificationManager? = null
+    init {
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createNotificationChannel()
+    }
 
     companion object {
         val WATCH_MOVIE_NOTIFICATION_CHANNEL_ID:String = "WMNCID"
@@ -23,7 +25,24 @@ class NotificationHandler(context:Context): ContextWrapper(context) {
         notificationChannel.enableLights(true)
         notificationChannel.lightColor = Color.RED
         notificationChannel.setShowBadge(true)
-        notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
-        notificationManager.createNotificationChannel(notificationChannel)
+        //notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+        notificationChannel.enableVibration(true)
+        notificationChannel.vibrationPattern = longArrayOf(100, 200, 300, 400, 500, 400, 300, 200, 400)
+
+        notificationManager?.createNotificationChannel(notificationChannel)
+    }
+
+    fun createAndReturnWatchMovieNotification(titleText:String, bodyText:String):Notification{
+        return Notification.Builder(applicationContext,WATCH_MOVIE_NOTIFICATION_CHANNEL_ID)
+                .setContentTitle(titleText)
+                .setContentText(bodyText)
+                .setSmallIcon(R.drawable.watch_movie)
+                .setAutoCancel(true)
+                .setChannelId(WATCH_MOVIE_NOTIFICATION_CHANNEL_ID)
+                .build()
+    }
+
+    fun notifyTheUser(notificationID:Int,notificationBuilder:Notification){
+        notificationManager?.notify(notificationID,notificationBuilder)
     }
 }
